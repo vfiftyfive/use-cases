f286db0e1c8027e43e3eed79284a6603781fe4fe
Ferran <ferran.castell@storageos.com>
2021-03-24 12:01:35 +0000

    Postgress migration examples

--- postgres/migration/import-data/patch.sh
+++ postgres/migration/import-data/patch.sh
@@ -1,6 +1,6 @@
-for i in {1..10}; do
-    k patch cm/postgres-migration \
+for i in {0..9}; do
+    kubectl patch cm/postgres-migration \
       -n app-$i \
       --type merge \
-      -p "{\"data\":{\"DB\": \"app_$i\"}}"
+      -p "{\"data\":{\"DB\": \"app-$i\"}}"
 done
